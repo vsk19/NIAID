9bd75285db86074d901827d812e5b3a06aa6cef0
arunbodd <arunbodd@outlook.com>
2019-02-08 10:13:04 -0500

    Update MultiQC.sh

--- WES_QC/MultiQC.sh
+++ WES_QC/MultiQC.sh
@@ -12,5 +12,5 @@ cd $1/$2
 cp /hpcdata/dir/SCRIPTS/MultiQC/Multiqc.snakemake /hpcdata/dir/SCRIPTS/MultiQC/MultiQC.sh /hpcdata/dir/SCRIPTS/MultiQC/run.json /hpcdata/dir/SCRIPTS/MultiQC/cluster.json $1/$2
 
 module load snakemake
-CLUSTER_OPTS="qsub -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -wd $1/$2/$3 walltime=24:00:00"
+CLUSTER_OPTS="qsub -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -l h_rt=24:00:00 -wd $1/$2/$3"
 snakemake -j 30 --cluster-config cluster.json --cluster "$CLUSTER_OPTS" --keep-going --snakefile Multiqc.snakemake > log.txt 2>&1 &
