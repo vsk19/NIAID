f7049a88bb0fd286441d9c0336de663e6d773848
arunbodd <arunbodd@outlook.com>
2019-02-14 11:32:56 -0500

    Update MultiQC.sh

--- WES_QC/MultiQC.sh
+++ WES_QC/MultiQC.sh
@@ -9,7 +9,7 @@ cd $1/$2
 mkdir $3
 cd $1/$2
 
-cp /hpcdata/dir/SCRIPTS/MultiQC/Multiqc.snakemake /hpcdata/dir/SCRIPTS/MultiQC/MultiQC.sh /hpcdata/dir/SCRIPTS/MultiQC/run.json /hpcdata/dir/SCRIPTS/MultiQC/cluster.json $1/$2
+cp /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/Multiqc.snakemake /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/MultiQC.sh /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/run.json /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/cluster.json $1/$2
 
 module load snakemake
 CLUSTER_OPTS="qsub -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -l h_rt=24:00:00 -wd $1/$2/$3"
