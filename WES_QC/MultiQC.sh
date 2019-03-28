67c16e7decc49e24ae0a441dd1786790e54feffd
Arun Kumar Boddapati <boddapatia2@ai-hpcn129.niaid.nih.gov>
2019-03-28 10:59:17 -0400

    Final commit

--- WES_QC/MultiQC.sh
+++ WES_QC/MultiQC.sh
@@ -9,8 +9,9 @@ cd $1/$2
 mkdir $3
 cd $1/$2
 
-cp /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/Multiqc.snakemake /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/MultiQC.sh /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/run.json /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/cluster.json $1/$2
+cp /hpcdata/scratch/boddapatia2/NIAID/WES_QC/CreateSnakejobReport.sh /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/Multiqc.snakemake /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/MultiQC.sh /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/run.json /hpcdata/dir/SCRIPTS/NCBR_github/NIAID/WES_QC/cluster.json $1/$2
+
 
 module load snakemake
 CLUSTER_OPTS="qsub -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -l virtual_free={cluster.vmem} -wd $1/$2/$3"
-snakemake -k --stats snakemake.stats --rerun-incomplete --restart-times 10 -j 100  --cluster-config cluster.json --cluster "$CLUSTER_OPTS" --keep-going --snakefile Multiqc.snakemake --report $2.snakemakereport.html > log.txt 2>&1 &
+snakemake -k --stats snakemake.stats --rerun-incomplete --restart-times 10 -j 100  --cluster-config cluster.json --cluster "$CLUSTER_OPTS" --keep-going --snakefile Multiqc.snakemake > log.txt 2>&1 &
