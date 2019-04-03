5e540f27280be4691727c50e6ab865359f31a6e9
Arun Kumar Boddapati <boddapatia2@ai-hpcn097.niaid.nih.gov>
2019-04-03 10:21:00 -0400

    Updated Rscripts-Goodtorunonallbatches

--- WES_QC/Variant_metrics_barplot.R
+++ WES_QC/Variant_metrics_barplot.R
@@ -1,5 +1,10 @@
 library(ggplot2)
-png("BATCH12_Het2Hom_mqc.png",width = 1000)
-metrics<-read.table("BATCH12.variant_calling_detail_metrics",header = TRUE,skip = 6)
+wd = getwd()
+
+split = strsplit(wd,"/")[[1]][[5]]
+
+png(paste0(split,"_Het2Hom_mqc.png"),width = 12,
+	height=6,units="in",res=1200)
+metrics<-read.table(paste0(split,".","variant_calling_detail_metrics"),header = TRUE,skip = 6)
 ggplot(data=metrics, aes(x=SAMPLE_ALIAS, y=HET_HOMVAR_RATIO)) + geom_bar(aes(color=HET_HOMVAR_RATIO<1.5),stat="identity",fill="steelblue") + theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=5))
 dev.off()
--- WES_QC/vcftools_barplot.R
+++ WES_QC/vcftools_barplot.R
@@ -1,9 +1,17 @@
 #install.packages("ggplot2")
 library(ggplot2)
 
-batch = read.table("BATCH12.het", header=T)
+wd = getwd()
 
-png("Inbreeding_Coefficient_mqc.png",res =600)
+split = strsplit(wd,"/")[[1]][[5]]
+
+batch = read.table(paste0(split,".","het"), header=T)
+
+png("test.png",width= 7,
+  height    = 4,
+  units     = "in",
+  res       = 1200,
+  pointsize = 2)
 
 ggplot(data=batch, aes(x=INDV, y=F)) + geom_bar(aes(color=F>0.1),stat="identity",fill="steelblue") + theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=3)) + ylim(-0.5,0.5)
 
