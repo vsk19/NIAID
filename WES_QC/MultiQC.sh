bb5914f4e7eb03803a18d8041f0ff5155fbff55e
Arun Kumar Boddapati <boddapatia2@ai-submit1.niaid.nih.gov>
2019-06-11 15:42:58 -0400

    changed path to snakefiles

--- WES_QC/MultiQC.sh
+++ WES_QC/MultiQC.sh
@@ -9,7 +9,7 @@ cd $1/$2
 mkdir $3
 cd $1/$2
 
-cp /nethome/boddapatia2/NIAID/WES_QC/Variant_metrics_barplot.R /nethome/boddapatia2/NIAID/WES_QC/vcftools_barplot.R /nethome/boddapatia2/NIAID/WES_QC/CreateSnakejobReport.sh /nethome/boddapatia2/NIAID/WES_QC/Multiqc.snakemake /nethome/boddapatia2/NIAID/WES_QC/MultiQC.sh /nethome/boddapatia2/NIAID/WES_QC/run.json /nethome/boddapatia2/NIAID/WES_QC/cluster.json $1/$2
+cp /hpcdata/dir/NCBR/NIAID/WES_QC/Variant_metrics_barplot.R /hpcdata/dir/NCBR/NIAID/WES_QC/vcftools_barplot.R /hpcdata/dir/NCBR/NIAID/WES_QC/CreateSnakejobReport.sh /hpcdata/dir/NCBR/NIAID/WES_QC/Multiqc.snakemake /hpcdata/dir/NCBR/NIAID/WES_QC/MultiQC.sh /hpcdata/dir/NCBR/NIAID/WES_QC/run.json /hpcdata/dir/NCBR/NIAID/WES_QC/cluster.json $1/$2
 
 
 module load snakemake
