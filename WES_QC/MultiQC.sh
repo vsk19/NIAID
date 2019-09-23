35734f9804dec2192c7e910fcb8703040e1ad364
Arun Kumar Boddapati <boddapatia2@ai-submit1.niaid.nih.gov>
2019-09-23 14:05:27 -0400

    modified behaviour of qsub submissions

--- WES_QC/MultiQC.sh
+++ WES_QC/MultiQC.sh
@@ -4,14 +4,13 @@
 
 
 cd $1
-mkdir $2
+mkdir -p $2
 cd $1/$2
-mkdir $3
+mkdir -p $3
 cd $1/$2
 
 cp /hpcdata/dir/NCBR/NIAID/WES_QC/Variant_metrics_barplot.R /hpcdata/dir/NCBR/NIAID/WES_QC/vcftools_barplot.R /hpcdata/dir/NCBR/NIAID/WES_QC/CreateSnakejobReport.sh /hpcdata/dir/NCBR/NIAID/WES_QC/Multiqc.snakemake /hpcdata/dir/NCBR/NIAID/WES_QC/MultiQC.sh /hpcdata/dir/NCBR/NIAID/WES_QC/run.json /hpcdata/dir/NCBR/NIAID/WES_QC/cluster.json $1/$2
 
-
-module load snakemake
-CLUSTER_OPTS="qsub -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -l virtual_free={cluster.vmem} -wd $1/$2/$3"
-snakemake -k --stats snakemake.stats --rerun-incomplete --restart-times 10 -j 100  --cluster-config cluster.json --cluster "$CLUSTER_OPTS" --keep-going --snakefile Multiqc.snakemake > log.txt 2>&1 &
+module load snakemake/5.3.0-Python-3.5.5
+CLUSTER_OPTS="qsub -sync y -pe threaded {cluster.threads} -l h_vmem={cluster.mem} -l virtual_free={cluster.vmem} -o $1/$2/$3/ -e $1/$2/$3/ -cwd"
+snakemake -k --stats snakemake.stats --rerun-incomplete --restart-times 10 -j 100 --cluster-config cluster.json --cluster-sync "$CLUSTER_OPTS" --keep-going --snakefile Multiqc.snakemake > log.snakemake.txt 2>&1 &
